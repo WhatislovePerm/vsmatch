ec20ddb2e0a7d675e2cc1715e06c21f3e73d9b5e
Vladslav Andreev <vvandreev.vl@gmail.com>
2026-06-28 12:11:53 +0300

    Add migration for UserRating default 750->500 (fix startup PendingModelChanges)

---
+++ VSMatch/Data/Migrations/20260628091107_LowerInitialRatingDefaultTo500.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VSMatch.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LowerInitialRatingDefaultTo500 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "UserRatings",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 500.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldDefaultValue: 750.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "UserRatings",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 750.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldDefaultValue: 500.0);
+        }
+    }
+}
